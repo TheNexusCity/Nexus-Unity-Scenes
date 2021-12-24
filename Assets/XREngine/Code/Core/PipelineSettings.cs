95df88d1599ef0345214cf6906210fced065f9be
dinomut1 <heximhotep@gmail.com>
2021-12-23 16:38:23 -0800

    lightmap support partially working. added config file to save export settings. more realitypack organization

--- Assets/XREngine/Code/Core/PipelineSettings.cs
+++ Assets/XREngine/Code/Core/PipelineSettings.cs
@@ -2,10 +2,12 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEditor;
 
 using System.IO;
 using System.Text.RegularExpressions;
 
+using Newtonsoft.Json;
 
 namespace XREngine
 {
@@ -16,23 +18,81 @@ namespace XREngine
         BAKE_SEPARATE
     }
 
-    [System.Serializable]
-    public class PipelineSettings
+    
+
+    [InitializeOnLoad]
+    public static class PipelineSettings
     {
+        [System.Serializable]
+        struct Data
+        {
+            public string GLTFName;
+            public string XREProjectFolder;
+            public bool ExportColliders;
+            public bool ExportSkybox;
+            public LightmapMode lightmapMode;
+            public int CombinedTextureResolution;
+            public void Apply()
+            {
+                PipelineSettings.GLTFName = GLTFName;
+                PipelineSettings.XREProjectFolder = this.XREProjectFolder;
+                PipelineSettings.ExportColliders = this.ExportColliders;
+                PipelineSettings.ExportSkybox = this.ExportSkybox;
+                PipelineSettings.lightmapMode = this.lightmapMode;
+                PipelineSettings.CombinedTextureResolution = this.CombinedTextureResolution;
+            }
+            public void Set()
+            {
+                GLTFName = PipelineSettings.GLTFName;
+                XREProjectFolder = PipelineSettings.XREProjectFolder;
+                ExportColliders = PipelineSettings.ExportColliders;
+                ExportSkybox = PipelineSettings.ExportSkybox;
+                lightmapMode = PipelineSettings.lightmapMode;
+                CombinedTextureResolution = PipelineSettings.CombinedTextureResolution;
+            }
+        }
 
-        public static string ConversionFolder = Application.dataPath + "/../Outputs/GLTF/";
+        public static readonly string ConversionFolder = Application.dataPath + "/../Outputs/GLTF/";
+        public static readonly string PipelineFolder = Application.dataPath + "/../Pipeline/";
+        public static readonly string configFile = PipelineFolder + "settings.conf";
+
+        public static string GLTFName;
         public static string XREProjectFolder;// = Application.dataPath + "/../Outputs/GLB/";
-
-        public static string PipelineFolder = Application.dataPath + "/../Pipeline/";
+        public static string XRELocalPath => "https://localhost:8642/" + 
+            Regex.Match(XREProjectFolder, @"[\w-]+[\\/]+[\w-]+[\\/]*$").Value;
+        
         public static bool ExportColliders;
         public static bool ExportSkybox;
 
-
         public static LightmapMode lightmapMode;
-        
-        
 
         public static int CombinedTextureResolution = 4096;
+        
+        static PipelineSettings()
+        {
+            ReadSettingsFromConfig();
+        }
+
+        public static void ReadSettingsFromConfig()
+        {
+            var config = new FileInfo(configFile);
+            if (!config.Exists)
+            {
+                return;
+            }
+            var data = JsonConvert.DeserializeObject<Data>
+            (
+                File.ReadAllText(configFile)
+            );
+            data.Apply();
+        }
+
+        public static void SaveSettings()
+        {
+            var data = new Data();
+            data.Set();
+            File.WriteAllText(configFile, JsonConvert.SerializeObject(data, Formatting.Indented));
+        }
 
         internal static void ClearPipelineJunk()
         {
--- Assets/XREngine/Code/GLTF/RealityPack/RPComponent.cs
+++ Assets/XREngine/Code/GLTF/RealityPack/RPComponent.cs
@@ -1,3 +1,4 @@
+using Newtonsoft.Json.Linq;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -7,7 +8,7 @@ namespace XREngine
     {
         public virtual string Type => "realitypack";
 
-        
+        public virtual JProperty Serialized => null;
     }
 
 }
